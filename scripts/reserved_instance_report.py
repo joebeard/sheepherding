b7560b015c9f6415882823e2cb48e5d12525cf81
Joe Beard <joe_beard@ziffdavis.com>
2015-10-19 09:59:17 +0100

    scripts/reserved_instance_report.py: add GNU header, improve HTML format

--- scripts/reserved_instance_report.py
+++ scripts/reserved_instance_report.py
@@ -1,24 +1,35 @@
 # reserved_instance_report.py
 #
-# jbeard 2015
+#    Author: Joe Beard <joe.beard@4a42.org>
 #
-# This lambda script has the following configuration:
-# * Run with a IAM role that has the following permissions
-#   * Describe all Reserved Instances in all regions specified
-#   * Send email using SES
-# * Scheduled to run periodically
-# * Timeout should be increased to 30s (its time to run is dependent on number of reserved instances)
-# * 
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License version 3, as
+#    published by the Free Software Foundation.
 #
-# Customisation
-# * regions - remove any regions you don't required
-# * tags_of_interest - any tags that you would like to appear in the report
-# * red_warning_days - days left to trigger red warning (default 30)
-# * orange_warning_days - days left to trigger orange warning (default 60)
-# * report_title - report title
-# * subject_string - email subject
-# * from_address - address from which report is sent
-# * to_address - address to which report is sent
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    This lambda script has the following configuration requirements:
+#    * Run with a IAM role that has the following permissions
+#      * Describe all Reserved Instances in all regions specified
+#      * Send email using SES
+#    * Scheduled to run periodically (it will not react to the event info supplied)
+#    * Timeout should be increased to 30s (its time to run is dependent on number of reserved instances)
+#
+#    Customisation
+#    * regions - remove any regions you don't required
+#    * tags_of_interest - any tags that you would like to appear in the report
+#    * red_warning_days - days left to trigger red warning (default 30)
+#    * orange_warning_days - days left to trigger orange warning (default 60)
+#    * report_title - report title
+#    * subject_string - email subject
+#    * from_address - address from which report is sent
+#    * to_address - address to which report is sent
 
 import json
 import boto3
@@ -114,15 +125,15 @@ def lambda_handler(event, context):
     if len(red_warning_reservations):
         html_msg+="<h2>Warning the following will expire within %s days!</h2>" % red_warning_days
         for row in red_warning_reservations:
-            html_msg += "<h2>Warning %s (%s) expires in less than 30 days (%s)</h2>" % (row[0], row[1], row[2])
+            html_msg += "<p>%s (%s) expires in %s days.</p>" % (row[0], row[1], row[2])
     if len(orange_warning_reservations):
         html_msg+="<h3>Warning the following will expire within %s days!</h3>" % orange_warning_days
         for row in orange_warning_reservations:
-            html_msg += "<p>Warning %s (%s) expires in less than 30 days (%s)</p>" % (row[0], row[1], row[2])
+            html_msg += "<p>%s (%s) expires in %s days.</p>" % (row[0], row[1], row[2])
     if len(expired_warning_reservations):
         html_msg+="<h3>The following have expired in the last 10 days.</h3>"
         for row in expired_warning_reservations:
-            html_msg += "<p>Warning %s (%s) expires in less than 30 days (%s)</p>" % (row[0], row[1], row[2])    
+            html_msg += "<p>%s (%s) expired %s days ago</p>" % (row[0], row[1], abs(row[2]))    
             
     html_msg += "<h4>All Reservations</h4>"
     html_msg += list_to_html_table(report_list)
